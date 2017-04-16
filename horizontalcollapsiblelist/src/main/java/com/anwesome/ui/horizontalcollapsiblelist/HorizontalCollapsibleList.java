330188e2a8c1318c296d522a50c65967bc84f455
Anwesh43 <anweshthecool0@gmail.com>
2017-04-16 20:11:56 +0530

    Adding HorizontalCollapsibleList

---
+++ horizontalcollapsiblelist/src/main/java/com/anwesome/ui/horizontalcollapsiblelist/HorizontalCollapsibleList.java
@@ -0,0 +1,32 @@
+package com.anwesome.ui.horizontalcollapsiblelist;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+
+/**
+ * Created by anweshmishra on 16/04/17.
+ */
+public class HorizontalCollapsibleList {
+    private Activity activity;
+    private ScrollView scrollView;
+    private ListLayout listLayout;
+    public HorizontalCollapsibleList(Activity activity) {
+        this.activity = activity;
+        listLayout = new ListLayout(activity);
+    }
+    public void addItem(Bitmap bitmap,String title) {
+        if(listLayout!=null && scrollView == null) {
+            listLayout.addItem(bitmap,title);
+        }
+    }
+    public void show() {
+        if(listLayout!=null && scrollView == null) {
+            scrollView = new ScrollView(activity);
+            scrollView.addView(listLayout,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            activity.setContentView(listLayout);
+
+        }
+    }
+}
