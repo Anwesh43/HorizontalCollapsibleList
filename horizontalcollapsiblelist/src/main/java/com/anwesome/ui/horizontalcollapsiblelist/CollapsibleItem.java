a1e4e3ffae3b63a226559a0ff972bfaea5f30005
Anwesh43 <anweshthecool0@gmail.com>
2017-04-16 22:03:01 +0530

    Drawing changes in collapsible item

--- horizontalcollapsiblelist/src/main/java/com/anwesome/ui/horizontalcollapsiblelist/CollapsibleItem.java
+++ horizontalcollapsiblelist/src/main/java/com/anwesome/ui/horizontalcollapsiblelist/CollapsibleItem.java
@@ -6,6 +6,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -27,26 +28,37 @@ public class CollapsibleItem extends View {
         this.title = title;
     }
     public void onDraw(Canvas canvas) {
+
         int w = canvas.getWidth(),h = canvas.getHeight();
+        paint.setColor(Color.WHITE);
+        canvas.drawRect(new RectF(0,0,w/5,h),paint);
         if(time == 0) {
-            bitmap = Bitmap.createScaledBitmap(bitmap,w/10,w/10,true);
-            collapsibleButton = new CollapsibleButton(w/10+w/20,h/2,w/10);
+            bitmap = Bitmap.createScaledBitmap(bitmap,w/20,w/20,true);
+            collapsibleButton = new CollapsibleButton(w/10+w/20+w/40,h/2,w/20);
         }
+        paint.setTextSize(h/3);
         canvas.save();
         Path path = new Path();
-        path.addCircle(0,h/2,w/20, Path.Direction.CCW);
+        path.addCircle(w/20,h/2,w/30, Path.Direction.CCW);
         canvas.clipPath(path);
-        canvas.drawBitmap(bitmap,0,h/2-w/20,paint);
+        canvas.drawBitmap(bitmap,w/20-w/40,h/2-w/40,paint);
         canvas.restore();
+        paint.setColor(Color.BLACK);
+        paint.setStrokeWidth(3);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawPath(path,paint);
+        paint.setStyle(Paint.Style.FILL);
         collapsibleButton.draw(canvas,paint);
         canvas.save();
         canvas.translate(w/5,h/2);
-        canvas.scale(scale,1);
         paint.setColor(Color.WHITE);
-        canvas.drawRect(0,-h/2,4*w/5,h/2,paint);
+        Path rectPath = new Path();
+        rectPath.addRect(new RectF(0,-h/2,(4*w/5)*scale,h/2), Path.Direction.CCW);
+        canvas.clipPath(rectPath);
+        canvas.drawPath(rectPath,paint);
         paint.setColor(Color.BLACK);
         String newTitle = adjustString(w/2);
-        canvas.drawText(newTitle,3*w/5-paint.measureText(newTitle)/2,paint.getTextSize()/6,paint);
+        canvas.drawText(newTitle,w/2-paint.measureText(newTitle)/2,paint.getTextSize()/6,paint);
         canvas.restore();
         time++;
         if(isAnimated) {
@@ -56,7 +68,7 @@ public class CollapsibleItem extends View {
                 isAnimated = false;
             }
             try {
-                Thread.sleep(100);
+                Thread.sleep(50);
                 invalidate();
             } catch (Exception ex) {
 
@@ -111,7 +123,7 @@ public class CollapsibleItem extends View {
             return dir == 0;
         }
         public boolean handleTap(float x,float y) {
-            boolean condition = x>=this.x && x<=this.x+w && y>=this.y && y<=this.y+w;
+            boolean condition = x>=this.x-w && x<=this.x+w && y>=this.y-w && y<=this.y+w;
             if(condition) {
                 dir = deg == 0?1:-1;
             }
