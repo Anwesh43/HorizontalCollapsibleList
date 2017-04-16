5311e14951155a01fe84a09685976dbefae76825
Anwesh43 <anweshthecool0@gmail.com>
2017-04-16 20:07:42 +0530

    add item in listlayout

--- horizontalcollapsiblelist/src/main/java/com/anwesome/ui/horizontalcollapsiblelist/ListLayout.java
+++ horizontalcollapsiblelist/src/main/java/com/anwesome/ui/horizontalcollapsiblelist/ListLayout.java
@@ -1,6 +1,7 @@
 package com.anwesome.ui.horizontalcollapsiblelist;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Point;
 import android.hardware.display.DisplayManager;
 import android.view.Display;
@@ -24,6 +25,11 @@ public class ListLayout extends ViewGroup {
             w = size.x;
             h = size.y;
         }
+    }
+    public void addItem(Bitmap bitmap,String title) {
+        CollapsibleItem collapsibleItem = new CollapsibleItem(getContext(),bitmap,title);
+        addView(collapsibleItem,new LayoutParams(9*w/10,Math.min(w,h)/10));
+        requestLayout();
     }
     public void onLayout(boolean reloaded,int a,int b,int w,int h) {
         int y = h/40;
