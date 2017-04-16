882fa964975c57df4ac85c86c882659170250dd0
Anwesh43 <anweshthecool0@gmail.com>
2017-04-16 22:05:49 +0530

    Using HorizontalCollapsible List in app

--- app/src/main/java/com/anwesome/ui/horizontalcollapsiblelistdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/horizontalcollapsiblelistdemo/MainActivity.java
@@ -1,13 +1,22 @@
 package com.anwesome.ui.horizontalcollapsiblelistdemo;
 
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.anwesome.ui.horizontalcollapsiblelist.HorizontalCollapsibleList;
 
+public class MainActivity extends AppCompatActivity {
+    private int images[] = {R.drawable.delivered,R.drawable.onway,R.drawable.order};
+    private String titles[] = {"delivered","onway","order"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        HorizontalCollapsibleList horizontalCollapsibleList = new HorizontalCollapsibleList(this);
+        for(int i=0;i<images.length;i++) {
+            horizontalCollapsibleList.addItem(BitmapFactory.decodeResource(getResources(),images[i]),titles[i]);
+        }
+        horizontalCollapsibleList.show();
     }
 }
